68c93bc3ea8052f258354fc297148f80b6de6242
Ali <aendris@uncc.edu>
2020-01-23 21:06:52 +0000

    RubyBasics1 - Part 2

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -1,13 +1,21 @@
 # Lab 1
 # Part I
 def sum arr
-  # YOUR CODE HERE
+  # YOUR CODE HEREc
+  
   return arr.inject(0, :+)
 end
 
 # Part II
 def max_2_sum arr
+  if arr.empty?
+    return 0
+  elsif arr.length == 1
+  return arr[0]
+  else
+    return arr.sort.last(2).reduce(:+)
   # YOUR CODE HERE
+  end
 end
 
 # Part III
