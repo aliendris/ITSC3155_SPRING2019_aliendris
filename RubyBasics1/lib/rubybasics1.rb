d4253df41a8b94f03265ef4a51010015e1944584
Ali <aendris@uncc.edu>
2020-01-24 05:15:39 +0000

    RubyBasics1.rb - Part 3

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -8,17 +8,20 @@ end
 
 # Part II
 def max_2_sum arr
+   # YOUR CODE HERE
   if arr.empty?
     return 0
   elsif arr.length == 1
   return arr[0]
   else
     return arr.sort.last(2).reduce(:+)
-  # YOUR CODE HERE
+ 
   end
 end
 
 # Part III
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+   # YOUR CODE HERE
+  arr.combination(2).any? {|a, b| a + b == n}
+ 
 end
