470122a000122bac80d82486bad175e7444735d8
Ali <aendris@uncc.edu>
2020-02-01 20:36:58 +0000

    RubyBasics 2 - part 2

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -3,11 +3,34 @@
 # Part I
 def hello(name)
   # YOUR CODE HERE
+    return "Hello, " + name
 end
 
 # Part II
 def starts_with_consonant? s
+  # /^[AaEeIiOoUu]/i.match(s) = nil
+  arraychar = s[0]
+  if (arraychar =~ /[bcdfghjklmnpqrstvwxyz]+/i)
+    return true
+  else 
+    return false
+  end 
+
+  # if(s[0].match arraychar) 
+  #   return true
+  # else
+  #   return nil
+   
+  # end
   # YOUR CODE HERE
+#   unless (s.empty?)
+#   return nil
+# else
+#   arrychar = s[0]
+#   
+#   end
+    
+#   end
 end
 
 # Part III
