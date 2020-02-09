cd8e45aff2162711f0ff3a273d62dcd359f5f082
Ali <aendris@uncc.edu>
2020-02-09 22:36:11 +0000

    Lab 9: Fun With Words - count words

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -1,8 +1,11 @@
 module FunWithStrings
   def palindrome?
   
- #ignoring non word character 
-  s = self.downcase.gsub /[^a-z0-9]/, ''
+ # /[^a-z0-9]/ ignoring non word caracter 
+ # downcase = converte the input string into lower case caracter 
+ #gsub = returns the string with all occurence 
+  s = self.downcase.gsub( /[^a-z0-9]/, '')
+  #compare the string s and the reverse string s matches 
   if(s == s.reverse)
     return true
   else
@@ -12,15 +15,26 @@ module FunWithStrings
     # your code here
   end
   def count_words
-    # your code here
+    @@wordcount = count_words
+    
+   
+  #   # your code here
+  
+   wordcount = self.downcase.gsub( /[^a-z0-9]/, '').text.split(" ")
+   wordcount = Hash.new(0)
+   wordcount.each do |index|
+   wordcount[index] += 1
+   end
   end
-  def anagram_groups
-    # your code here
-  end
+   wordcount
 end
-
+ def anagram_groups
+  #   # your code here
+ end
+ 
 # make all the above functions available as instance methods on Strings:
 
 class String
   include FunWithStrings
+
 end
