89a08449f99630ab85c2d8770974e0e2992d52e1
Ali <aendris@uncc.edu>
2020-02-12 22:30:39 +0000

    Ruby 00 Basics - Part 1

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -1,5 +1,10 @@
 # Ruby Basics Part 3
 
 class BookInStock
+    attr_accessor :isbn, :price
+    def initialize(isbn = "No ISBN provided", price = 0.0)
+        @isbn =isbn
+        @price =price
+    end
 # YOUR CODE HERE
 end
