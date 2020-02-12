12f848dfd387c53df4637a9e1e76bc5113a5c34c
Ali <aendris@uncc.edu>
2020-02-12 23:35:58 +0000

    Ruby 00 Basics - Part 2

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -1,10 +1,14 @@
 # Ruby Basics Part 3
 
 class BookInStock
+    # YOUR CODE HERE
+    #attr_accessor is used to creats reader and writer 
     attr_accessor :isbn, :price
     def initialize(isbn = "No ISBN provided", price = 0.0)
         @isbn =isbn
         @price =price
+         raise ArgumentError, 'ArgumentError' if(isbn =="" || price <=0)
     end
-# YOUR CODE HERE
+  
+
 end
