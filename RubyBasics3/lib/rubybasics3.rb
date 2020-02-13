809b28aa2d4666f9bda6cadb67ec70e323579990
Ali <aendris@uncc.edu>
2020-02-13 00:10:25 +0000

    Ruby 00 Basics -Part 3

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -7,8 +7,16 @@ class BookInStock
     def initialize(isbn = "No ISBN provided", price = 0.0)
         @isbn =isbn
         @price =price
+        
+        # part II to raise Argument Error when isbn is empty and price is zero or negatinve neumber 
          raise ArgumentError, 'ArgumentError' if(isbn =="" || price <=0)
+        
     end
+    
+     # part III 
+     def price_as_string
+         @price = price 
+         "$%.2f"%price
+     end
   
-
 end
